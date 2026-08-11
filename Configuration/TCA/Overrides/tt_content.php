45bbc349d0f65e556d0991cc773b2043c7738581
Christian Platt <christian.platt@pharmaline.de>
2026-08-11 21:37:59 +0200

    [Feature] CE Listing as Content Element

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,18 +8,34 @@ defined('TYPO3') or die();
 // Es existierten keine tt_content-Datensaetze mit phlorder_order, eine
 // Record-Migration war daher nicht noetig.
 //
-// Keine FlexForm mehr: die alte flexform_order.xml enthielt ausschliesslich
-// switchableControllerActions. Damit bleibt kein einziges Feld uebrig, das an ein
-// Plugin zu haengen waere - die Datei ist geloescht.
+// Die alte flexform_order.xml enthielt ausschliesslich switchableControllerActions
+// und wurde deshalb in Phase 6 geloescht. Die neue flexform_order.xml haengt nur am
+// Cockpit-Plugin und legt die Quelle der Bestellungen fest (settings.sourcePid);
+// ausgewertet in OrderController::initStoragePid().
 
 $pluginIcon = 'EXT:phlorder/Resources/Public/Icons/user_plugin_order.svg';
 
 // Plugin "Order" (Signatur phlorder_order) - Cockpit-/Listenansicht.
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$signatureOrder = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Phlorder',
     'Order',
     'Bestellung (Cockpit)',
     $pluginIcon
+);
+
+// FlexForm nur fuer das Cockpit-Plugin. Bei eigenen CTypes (kein list_type) laeuft
+// das ueber addToAllTCAtypes + addPiFlexFormValue mit dem CType als drittem
+// Argument - subtypes_addlist greift hier nicht.
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;Konfiguration,pi_flexform,',
+    $signatureOrder,
+    'after:subheader'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:phlorder/Configuration/FlexForms/flexform_order.xml',
+    $signatureOrder
 );
 
 // Plugin "Orderstatus" (Signatur phlorder_orderstatus) - Statusseite, ?t=<orderid>.
